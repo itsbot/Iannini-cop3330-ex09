8da72c4b7353b2f8fa0869aa3bdbe45916037a50
itsbot <patrickiannini@knights.ucf.edu>
2021-09-10 15:35:43 -0400

    Completed Exercise

--- src/main/java/Exercise09/Exercise09.java
+++ src/main/java/Exercise09/Exercise09.java
@@ -3,9 +3,22 @@
  *  Copyright 2021 Patrick Iannini
  */
 package Exercise09;
+import java.util.Scanner;
 
 public class Exercise09 {
     public static void main(String[] args) {
-        
+        System.out.println("Enter the width");
+        Scanner sc = new Scanner(System.in);
+        int width = sc.nextInt();
+
+        System.out.println("Enter the length");
+        Scanner sc1 = new Scanner(System.in);
+        int length = sc1.nextInt();
+
+        int sqftPerGal = 350;
+        int sqfeet = length*width;
+        int gal = (int) Math.ceil((double)sqfeet/sqftPerGal);
+
+        System.out.println("You will need to purchase "+gal+" gallons of paint to cover "+sqfeet+" square feet.");
     }
 }
